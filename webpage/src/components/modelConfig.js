3abcb1baf614f792e747956978e026b8eedc44ec
André Silva <andre15andre@hotmail.com>
2024-09-19 12:59:51 +0200

    add llama, update webpage

--- webpage/src/components/modelConfig.js
+++ webpage/src/components/modelConfig.js
@@ -2,9 +2,11 @@ export const modelList = [
     ['gemini-1.5-pro', "google", "Google"],
     ['gpt-4o-2024-08-06', "openai-chatcompletion", "OpenAI"],
     ['o1-preview-2024-09-12', "openai-chatcompletion", "OpenAI"],
+    ['llama-3.1-405b-instruct', 'openrouter', 'Meta'],
   ];
   
   export const llmCountryMap = {
     'Google': 'US',
     'OpenAI': 'US',
+    'Meta': 'US',
   };
