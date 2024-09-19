a34e0b6062fa77cf57919756bcab13ef54c20739
André Silva <andre15andre@hotmail.com>
2024-09-19 17:12:06 +0200

    update model config

--- webpage/src/components/modelConfig.js
+++ webpage/src/components/modelConfig.js
@@ -1,7 +1,7 @@
 export const modelList = [
     ['gemini-1.5-pro', "google", "Google"],
     ['gpt-4o-2024-08-06', "openai-chatcompletion", "OpenAI"],
-    ['o1-preview-2024-09-12', "openai-chatcompletion", "OpenAI"],
+    // ['o1-preview-2024-09-12', "openai-chatcompletion", "OpenAI"],
     ['llama-3.1-405b-instruct', 'openrouter', 'Meta'],
   ];
   
