b11293f73cdbce34fef59a511ee11593cd1dec79
André Silva <andre15andre@hotmail.com>
2024-09-19 11:17:18 +0200

    add google analytics

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,9 +1,17 @@
 // src/App.js
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 import LeaderboardTable from './components/LeaderboardTable';
 import './App.css';
 
+const MEASUREMENT_ID = 'G-DLB9F9YV03';
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(MEASUREMENT_ID);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <div className="App">
       <h1>RepairBench: Live Leaderboard of Frontier LLMs for Program Repair</h1>
