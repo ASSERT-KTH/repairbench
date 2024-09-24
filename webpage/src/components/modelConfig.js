a876b46ed52e704a3ab05fc5b1c122fdfb46c8e0
André Silva <andre15andre@hotmail.com>
2024-09-24 11:07:54 +0200

    update results

--- webpage/src/components/modelConfig.js
+++ webpage/src/components/modelConfig.js
@@ -1,10 +1,12 @@
 export const modelList = [
     ['gemini-1.5-pro', "google", "Google"],
     ['gpt-4o-2024-08-06', "openai-chatcompletion", "OpenAI"],
-    // ['o1-preview-2024-09-12', "openai-chatcompletion", "OpenAI"],
+    ['o1-preview-2024-09-12', "openai-chatcompletion", "OpenAI"],
     ['llama-3.1-405b-instruct', 'openrouter', 'Meta'],
     ['deepseek-v2.5', 'openrouter', 'DeepSeek'],
     ['mistral-large-2407', 'openrouter', 'Mistral'],
+    ['qwen-2.5-72b-instruct', 'openrouter', 'Alibaba Cloud'],
+    ['claude-3-5-sonnet-20240620', 'anthropic', 'Anthropic'],
   ];
   
   export const llmCountryMap = {
@@ -13,4 +15,6 @@ export const modelList = [
     'Meta': 'US',
     'DeepSeek': 'CN',
     'Mistral': 'FR',
+    'Alibaba Cloud': 'CN',
+    'Anthropic': 'US',
   };
