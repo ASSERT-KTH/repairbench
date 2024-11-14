84c278e8b1f5878a5369ee29c3c157b0ada2274f
André Silva <andre15andre@hotmail.com>
2024-11-14 12:14:51 +0100

    remove double github

--- webpage/src/components/Footer.js
+++ webpage/src/components/Footer.js
@@ -10,10 +10,7 @@ const Footer = () => {
       <a href="https://arxiv.org/pdf/2409.18952" target="_blank" rel="noopener noreferrer">
         <SiArxiv className="footer-icon" />
       </a>
-      <a href="https://github.com/ASSERT-KTH/repairbench" target="_blank" rel="noopener noreferrer">
-        <FaGithub className="footer-icon" />
-      </a>
-      <a href="https://github.com/ASSERT-KTH/elle-elle-aime" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/ASSERT-KTH/repairbench-framework" target="_blank" rel="noopener noreferrer">
         <FaGithub className="footer-icon" />
       </a>
     </div>
